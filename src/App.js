aa3a797439d2c99d8b15a484754d2e6f6f773853
Naomi Koh <nck23@cornell.edu>
2021-06-09 20:13:11 -0400

    basic structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import Header from "./component/header"
 import './App.css';
+import React, { useState } from 'react';
+import { TextField, Checkbox } from '@material-ui/core';
+import Table from "./component/table"
 
-function App() {
-  return (
-    <div className="App">
-    <Header/>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todolistitems: [],
+    };
+  }
+
+
+
+  render() {
+    return (
+      <div className="App" >
+        <Header />
+        <Table />
+        <Checkbox
+          color="primary"
+          inputProps={{ 'aria-label': 'secondary checkbox' }}
+        />
+        <TextField id="standard-basic" label="Standard" />
+      </div>
+    );
+  }
 }
 
 export default App;
