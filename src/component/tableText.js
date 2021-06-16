83f4b4c08124a1c8ffb650aa7a3bda646d22298d
Alexis C <alexis.cathcart0305@gmail.com>
2021-06-15 22:00:41 -0400

    (attempt at) creating textbox
    i have no clue what i am doing, and i do not even know if this works

---
+++ src/component/tableText.js
@@ -0,0 +1,83 @@
+import React, { useState } from 'react';
+import { Input } from '@material-ui/core';
+import { render } from 'react-dom';
+
+const TableText = (props) => {
+    return (
+        // via https://material-ui.com/components/text-fields/
+        <form className={classes.root} noValidate autoComplete="off">
+            <Input placeholder="Enter task" inputProps={{ 'aria-label': 'description' }} />
+        </form>
+    )
+};
+
+export default TableText;
+
+/*
+// via React Conf video
+
+export default class TableText extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            task: 'Enter task',
+        }
+        this.handleTaskChange = this.handleTaskChange.bind(this);
+    }
+
+    handleTaskChange(t) {
+        this.setState({
+            task: t.target.value
+        });
+    }
+
+    render() {
+        return (
+            <form className={classes.root} noValidate autoComplete="off">
+                <Input placeholder="Enter task" inputProps={{ 'aria-label': 'description' }} />
+            </form>
+        );
+    }
+}
+*/import React, { useState } from 'react';
+import { Input } from '@material-ui/core';
+import { render } from 'react-dom';
+
+const TableText = (props) => {
+    return (
+        // via https://material-ui.com/components/text-fields/
+        <form className={classes.root} noValidate autoComplete="off">
+            <Input placeholder="Enter task" inputProps={{ 'aria-label': 'description' }} />
+        </form>
+    )
+};
+
+export default TableText;
+
+/*
+// via React Conf video
+
+export default class TableText extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            task: 'Enter task',
+        }
+        this.handleTaskChange = this.handleTaskChange.bind(this);
+    }
+
+    handleTaskChange(t) {
+        this.setState({
+            task: t.target.value
+        });
+    }
+
+    render() {
+        return (
+            <form className={classes.root} noValidate autoComplete="off">
+                <Input placeholder="Enter task" inputProps={{ 'aria-label': 'description' }} />
+            </form>
+        );
+    }
+}
+*/
